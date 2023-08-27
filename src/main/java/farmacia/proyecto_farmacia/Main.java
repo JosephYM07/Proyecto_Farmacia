61d1230e47db304ccd1c02e5810e02687598a4da
Joseph YM <josepym07@gmail.com>
2023-08-26 19:46:53 -0500

    Se agrega avance pantalla admin

---
+++ src/main/java/farmacia/proyecto_farmacia/Main.java
@@ -0,0 +1,33 @@
+package farmacia.proyecto_farmacia;
+
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+public class Main extends Application {
+    private double x, y;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Pantalla_login_admin.fxml")); //Este sirve para cargar el archivo fxml
+        primaryStage.setScene(new Scene(root));// esto sirve para que se muestre la pantalla
+        primaryStage.initStyle(StageStyle.UNDECORATED); //Este sirve para quitar los botones de cerrar, minimizar y maximizar
+
+        //Este sirve para mover la ventana
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> { //Este sirve para mover la ventana
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+        primaryStage.show(); //Este sirve para mostrar la ventana
+    }
+
+    //Este es el main que se ejecuta al iniciar el programa
+    public static void main(String[] args) {
+        launch(args); //Este sirve para iniciar la aplicación
+    }
+}
---
+++ src/main/java/farmacia/proyecto_farmacia/Pantalla_login_admin.java
@@ -0,0 +1,48 @@
+package farmacia.proyecto_farmacia;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class Pantalla_login_admin {
+    @FXML
+    private VBox pnItems;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnCustomers1;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    public void handleClicks(ActionEvent actionEvent) {
+        // Aquí maneja los clics de los botones
+        if (actionEvent.getSource() == btnOverview) {
+            // Código para el botón Overview
+        } else if (actionEvent.getSource() == btnCustomers) {
+            // Código para el botón Customers
+        } else if (actionEvent.getSource() == btnCustomers1) {
+            // Código para el botón Customers1
+        } else if (actionEvent.getSource() == btnSettings) {
+            // Código para el botón Settings
+        } else if (actionEvent.getSource() == btnSignout) {
+            // Código para el botón Signout
+        }
+    }
+}
