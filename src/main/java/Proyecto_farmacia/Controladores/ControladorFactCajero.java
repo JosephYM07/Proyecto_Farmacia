ac80eb89767ee72ea12b585f20b2fbcce970acb9
Jared Valenzuela <jared.valenzuela@epn.edu.ec>
2023-09-11 19:24:25 -0500

    Creacion del modulo de factura cajero

--- src/main/java/Proyecto_farmacia/Controladores/ControladorFactCajero.java
+++ src/main/java/Proyecto_farmacia/Controladores/ControladorFactCajero.java
@@ -1,4 +1,21 @@
 package Proyecto_farmacia.Controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControladorFactCajero {
+    private double x, y;
+    //PANEL
+    @FXML
+    private Button BotonHome;
+    @FXML
+    private Button BotonCajeros;
+    @FXML
+    private Button BotonAdmin;
+    @FXML
+    private Button BotonSalir;
+    @FXML
+    private Button BotonMinimizarApp;
+    @FXML
+    private Button BotonCerrarApp;
 }
